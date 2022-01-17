3051d1ff05efe4bb0303393ab71043e72ab969f0
ADamian17 <adonisdesuvikdo@gmail.com>
2022-01-16 20:30:49 -0800

    added get one movie

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -2,6 +2,15 @@ const movies = async (parent, args, context) => {
   return await context.prisma.movie.findMany()
 }
 
+const movie = async (parent, args, context) => {
+  return await context.prisma.movie.findUnique({
+    where: {
+      id: Number(args.id)
+    }
+  })
+}
+
 module.exports = {
-  movies
+  movies,
+  movie
 }
